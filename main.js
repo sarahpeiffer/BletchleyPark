a8fca220243db99321b67106df197de0231b5e2b
sarahpeiffer <46665092+sarahpeiffer@users.noreply.github.com>
2021-01-05 19:11:30 -0600

    save progress

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const imageArray = ["intro", "about_us", "team", "colossus"];
 let index = 0;
-window.onload=function(){
-    setInterval(function(){switchImage()},5000) //6 seconds
+window.onload = function () {
+    setInterval(function () { switchImage() }, 5000) //6 seconds
 }
 
 function switchImage() {
-    document.getElementById("rotate").innerHTML = "";
-    if(index == imageArray.length - 1) {
+    if (index == imageArray.length - 1) {
         index = 0;
     }
     else {
         index++;
     }
-   let newImage = document.createElement("img");
-    newImage.src = "images/" + imageArray[index] + ".jpg";
-    newImage.classList.add("column-image");
-    document.getElementById("rotate").append(newImage);
+    $('#rotate-image').fadeOut(500, function() {
+        $('#rotate-image').attr("src","images/" + imageArray[index] + ".jpg");
+        $('#rotate-image').fadeIn(500);
+    });
 }
